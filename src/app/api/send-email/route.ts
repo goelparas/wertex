695c7ee7601ae828bf73c1fda14a2edb8c30d2b9
paras.goel <paras.work101@gmail.com>
2025-02-10 01:29:55 +0530

    feat: connection for email update

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
-      secure: false, // Use STARTTLS instead of SSL
+      secure: true, // Use STARTTLS instead of SSL
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
