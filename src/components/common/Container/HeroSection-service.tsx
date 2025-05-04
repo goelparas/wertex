361ddc6440d2d5c6043d391458792ae17b0fdb03
manthan <manthan@outbox.vs>
2025-05-05 01:07:46 +0530

    refactor(services): consolidate and enhance service components

--- src/components/common/Container/HeroSection-service.tsx
+++ src/components/common/Container/HeroSection-service.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import FlexHeader from "../FlexHeader/FlexHeader";
-import Image, { StaticImageData } from "next/image";
+import React from 'react';
+import FlexHeader from '../FlexHeader/FlexHeader';
+import Image, { StaticImageData } from 'next/image';
 
 const HeroSection = ({
   src,
@@ -14,18 +14,18 @@ const HeroSection = ({
   alt: string;
   title: string;
   description: string;
-  heading: string;
+  heading?: string;
   longDescription: string;
 }) => {
   return (
-    <div className="w-[90%] mx-auto ">
+    <div className='w-[90%] mx-auto '>
       <FlexHeader title={title} description={description} heading={heading} />
-      <div className="w-full flex flex-col  justify-between items-center gap-8 h-[85svh] mt-12">
-        <div className="h-full relative w-full">
-          <Image src={src} alt={alt} quality={100} objectFit="cover" fill />
+      <div className='w-full flex flex-col  justify-between items-center gap-8 h-[85svh] mt-12'>
+        <div className='h-full relative w-full'>
+          <Image src={src} alt={alt} quality={100} objectFit='cover' fill />
         </div>
 
-        <p className="text-xl2 text-white w-full">{longDescription}</p>
+        <p className='text-xl2 text-white w-full'>{longDescription}</p>
       </div>
     </div>
   );
--- src/components/ui/services/cncmachine.tsx
+++ src/components/ui/services/cncmachine.tsx
@@ -1,15 +1,23 @@
+'use client';
 
+import CncCapabilitiesSection from '@/components/cnc/CncCapabilitiesSection';
+import CncFeaturesSection from '@/components/cnc/CncFeaturesSection';
+import CncHeroSection from '@/components/cnc/CncHeroSection';
+import CncPerformanceSection from '@/components/cnc/CncPerformanceSection';
+import CncWorkflowSection from '@/components/cnc/CncWorkflowSection';
+import IndustriesAndMaterialsSection from '@/components/cnc/IndustriesAndMaterialsSection';
+import Container from '@/components/common/Container/Container';
+import React from 'react';
 
-
-
-import React from 'react'
-
-type Props = {}
-
-const CncMachine = (props: Props) => {
+export default function CncPage() {
   return (
-    <div>cncmachine</div>
-  )
-};
-
-export default CncMachine;
+    <Container className='bg-black px-0 w-full'>
+      <CncHeroSection />
+      <CncCapabilitiesSection />
+      <IndustriesAndMaterialsSection />
+      <CncWorkflowSection />
+      <CncFeaturesSection />
+      <CncPerformanceSection />
+    </Container>
+  );
+}
