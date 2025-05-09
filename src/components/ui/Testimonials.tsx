5d060fa7c6e298167e88d79827d73b2b6e0c8fec
paras.goel <paras.work101@gmail.com>
2025-05-09 21:27:16 +0530

    update : testimonial

---
+++ src/components/ui/Testimonials.tsx
@@ -0,0 +1,84 @@
+import FlexHeader, { FlexHeader2 } from "../common/FlexHeader/FlexHeader";
+
+export default function TeamShowcase() {
+  const teamMembers = [
+    { id: "01", name: "Name 01", designation: "Designation & Bio" },
+    { id: "02", name: "Name 02", designation: "Designation & Bio" },
+    { id: "03", name: "Name 03", designation: "Designation & Bio" },
+    { id: "04", name: "Name 04", designation: "Designation & Bio" },
+    { id: "05", name: "Name 05", designation: "Designation & Bio" },
+    { id: "06", name: "Name 06", designation: "Designation & Bio" },
+  ];
+
+  const testimonials = [
+    {
+      id: 1,
+      quote:
+        "Great experience working with them —excellent quality control and customer service.",
+    },
+    {
+      id: 2,
+      quote:
+        "Every interaction with their team was seamless. They delivered exactly what we needed with great attention to detail.",
+    },
+    {
+      id: 3,
+      quote: "From design to delivery, every step was smooth and professional.",
+    },
+    {
+      id: 4,
+      quote:
+        "An outstanding team that balances professionalism with great communication. They truly understood our vision.",
+    },
+  ];
+
+  return (
+    <div className="bg-mutedGray  md:p-12 lg:p-12 min-h-dvh">
+      <div className="my-12">
+        <FlexHeader title="WHAT OUR" description="" heading="CLIENTS SAYS!" />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
+          {testimonials.map((testimonial) => (
+            <div
+              key={testimonial.id}
+              className="bg-mutedGray p-6 rounded-lg flex flex-col h-full"
+            >
+              <div className="w-12 h-12 bg-white rounded-full mb-6 "></div>
+              <p className="text-white text-lg leading-relaxed w-full h-44 p-4 clipped bg-[#282828]">
+                {testimonial.quote}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row justify-between my-16">
+        <FlexHeader
+          title="THE THINKERS, DOERS, AND INNOVATORS"
+          description="Driving innovation through expertise."
+          heading=""
+        />
+      </div>
+      <div className="flex w-full  items-start justify-between">
+        <div className="w-1/4">
+          <p className="text-xl2">
+            We've been fortunate to work with a lot of awesome people on even
+            more awesome projects.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-3/4">
+          {teamMembers.map((member, index) => (
+            <div key={member.id} className={`${index >= 3 ? "mt-8" : ""}`}>
+              <div className="flex items-center mb-4">
+                <div className="w-12 h-12 bg-gray-400 rounded-full mr-4"></div>
+                <div>
+                  <h3 className="font-medium text-xl">{member.name}</h3>
+                  <p className="text-gray-400">{member.designation}</p>
+                </div>
+              </div>
+              <div className="bg-gray-200 aspect-[4/3] w-full"></div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/ui/hero-section/hero-section.tsx
+++ src/components/ui/hero-section/hero-section.tsx
@@ -1,85 +1,107 @@
-import React from 'react'
-import CustomImage from "@/components/common/Image/Image"
-import logo from "@/cdn/images/logo.avif"
-import bgDesktop from "@/cdn/images/hero_background.avif"
-import bgMobile from "@/cdn/images/hero_background_mobile.avif"
-import Image from 'next/image';
-import WhatWeOffer from './what-we-offer'
-import { BrandsShowCase } from './brands-showcase'
-import { TESTIMONIALS } from '@/utils/constants/constant'
-import ServicesSection from './services-section'
-import Inovation from './inovation'
-import CountryWork from './country-work'
+import React from "react";
+import CustomImage from "@/components/common/Image/Image";
+import logo from "@/cdn/images/logo.avif";
+import bgDesktop from "@/cdn/images/hero_background.avif";
+import bgMobile from "@/cdn/images/hero_background_mobile.avif";
+import Image from "next/image";
+import WhatWeOffer from "./what-we-offer";
+import { BrandsShowCase } from "./brands-showcase";
+import { TESTIMONIALS } from "@/utils/constants/constant";
+import ServicesSection from "./services-section";
+import Inovation from "./inovation";
+import CountryWork from "./country-work";
+import Testimonials from "@/components/ui/Testimonials";
 
 const HeroSection = () => {
-    const statsData = [
-        { value: "38", label: "Year of Growth" },
-        { value: "3000+", label: "Projects completions" },
-        { value: "300+", label: "satisfied clients" }
-    ];
-    return (
-        <>
-            <div className="relative h-svh w-screen flex items-center justify-center border-b-[1.5px] border-[#3F3F3F] bg-background overflow-x-hidden">
-                <div className="absolute inset-0" >
-                    <CustomImage
-                        mobileImage={{ path: bgMobile }}
-                        desktopImage={{ path: bgDesktop }}
-                        fill
-                        alt="background"
-                    />
+  const statsData = [
+    { value: "38", label: "Year of Growth" },
+    { value: "3000+", label: "Projects completions" },
+    { value: "300+", label: "satisfied clients" },
+  ];
+  return (
+    <>
+      <div className="relative h-svh w-screen flex items-center justify-center border-b-[1.5px] border-[#3F3F3F] bg-background overflow-x-hidden">
+        <div className="absolute inset-0">
+          <CustomImage
+            mobileImage={{ path: bgMobile }}
+            desktopImage={{ path: bgDesktop }}
+            fill
+            alt="background"
+          />
+        </div>
+
+        <span className="absolute  bottom-6 mx-auto lg:hidden">
+          <a
+            className=" bg-custom-gradient font-bold text-xs sm:text-sm md:text-[1rem] px-6 py-3 sm:px-8 sm:py-4   clipped uppercase"
+            href="mailto:contact@wertex.in?subject=Query%20Regarding%20Manufacturing&body=Hello,%0A%0AI%20have%20some%20queries%20regarding%20your%20manufacturing%20process.%0A%0AThank%20you."
+          >
+            Contact us
+          </a>
+        </span>
+
+        <div className="absolute w-[20rem]  sm:w-[40rem] md:w-[49rem]   h-[12rem] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="relative flex flex-col sm:flex-row gap-2  items-start sm:items-center justify-between  mb-2">
+            <p className="font-medium text-xxs lg:text-xs">
+              Since <br />
+              2010
+            </p>
+            <figure className=" w-full relative  h-16 sm:h-[9rem] md:h-[10rem]">
+              <Image src={logo} alt="logo" quality={100} fill />
+            </figure>
+          </div>
+          <h1 className="font-medium text-sm sm:text-2xl text-center">
+            Shaping the Future of Manufacturing
+          </h1>
+        </div>
+        <div className="absolute m-2 sm:m-12  left-0 bottom-20 lg:bottom-5 right-0 ">
+          <div className="grid  grid-cols-3 gap-8 sm:gap-32  mx-[2%]">
+            {statsData.map((stat) => (
+              <div
+                className="flex items-center justify-center sm:px-2"
+                key={stat.value}
+              >
+                <div className="w-[12.5rem]">
+                  <h1 className="font-bold text-sm sm:text-3xl text-center">
+                    {stat.value}
+                  </h1>
+                  <p className="uppercase text-center text-[6px] sm:text-sm">
+                    {stat.label}
+                  </p>{" "}
                 </div>
-                
-                   
-                 
-                <span className='absolute  bottom-6 mx-auto lg:hidden'>
-                    <a className=" bg-custom-gradient font-bold text-xs sm:text-sm md:text-[1rem] px-6 py-3 sm:px-8 sm:py-4   clipped uppercase" href="mailto:contact@wertex.in?subject=Query%20Regarding%20Manufacturing&body=Hello,%0A%0AI%20have%20some%20queries%20regarding%20your%20manufacturing%20process.%0A%0AThank%20you.">Contact us</a>
-                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
 
-                <div className="absolute w-[20rem]  sm:w-[40rem] md:w-[49rem]   h-[12rem] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <div className='relative flex flex-col sm:flex-row gap-2  items-start sm:items-center justify-between  mb-2'>
-                        <p className='font-medium text-xxs lg:text-xs'>Since <br />2010</p>
-                        <figure className=' w-full relative  h-16 sm:h-[9rem] md:h-[10rem]'>
-                            <Image src={logo} alt="logo" quality={100} fill />
-                        </figure>
-                    </div>
-                    <h1 className="font-medium text-sm sm:text-2xl text-center">Shaping the Future of Manufacturing</h1>
-                </div>
-                <div className="absolute m-2 sm:m-12  left-0 bottom-20 lg:bottom-5 right-0 ">
-                    <div className="grid  grid-cols-3 gap-8 sm:gap-32  mx-[2%]">
-                        {
-                            statsData.map((stat) => (
-                                <div className='flex items-center justify-center sm:px-2' key={stat.value}>
-                                    <div className="w-[12.5rem]">
-                                        <h1 className="font-bold text-sm sm:text-3xl text-center">{stat.value}</h1><p className="uppercase text-center text-[6px] sm:text-sm">{stat.label}</p> </div>
-                                </div>))
-                        }
-                    </div>
+      <WhatWeOffer />
 
-                </div>
-            </div>
+      <div className="mt-8 border-t border-t-smokeWhite flex flex-col  gap-8 items-center justify-between p-8 w-full">
+        <div className="flex flex-col lg:flex-row justify-between w-full items-start lg:items-center py-6 gap-2">
+          <h1 className="font-bold lg:text-2xl uppercase hidden lg:block  ">
+            OUR TRACK RECORD OF <br /> TRUSTED PARTNERS AND CLIENTS
+          </h1>
+          <h1 className="font-bold text-xl  uppercase lg:hidden">
+            OUR TRACK RECORD OF TRUSTED PARTNERS AND CLIENTS
+          </h1>
+          <p className="font-normal  text-[10px] md:text-xs lg:text-lg  md:w-[30%] text-start">
+            We've been fortunate to work with a lot of awesome people on even
+            more awesome projects.
+          </p>
+        </div>
+        <div className="w-full">
+          <BrandsShowCase items={TESTIMONIALS.slice(0, 4)} />
+          <BrandsShowCase items={TESTIMONIALS.slice(4, 8)} direction="right" />
+          <BrandsShowCase items={TESTIMONIALS.slice(8)} />
+        </div>
+      </div>
+      <ServicesSection />
+      <Inovation />
 
-            <WhatWeOffer />
+      <CountryWork />
+      <Testimonials />
+    </>
+  );
+};
 
-            <div className='mt-8 border-t border-t-smokeWhite flex flex-col  gap-8 items-center justify-between p-8 w-full'>
-                <div className='flex flex-col lg:flex-row justify-between w-full items-start lg:items-center py-6 gap-2'>
-                    <h1 className='font-bold lg:text-2xl uppercase hidden lg:block  '>OUR TRACK RECORD OF <br /> TRUSTED PARTNERS AND CLIENTS</h1>
-                    <h1 className='font-bold text-xl  uppercase lg:hidden'>OUR TRACK RECORD OF  TRUSTED PARTNERS AND CLIENTS</h1>
-                    <p className='font-normal  text-[10px] md:text-xs lg:text-lg  md:w-[30%] text-start'>We've been fortunate to work with a lot of awesome people on even more awesome projects.</p>
-                </div>
-                <div className='w-full'>
-                    <BrandsShowCase items={TESTIMONIALS.slice(0, 4)} />
-                    <BrandsShowCase items={TESTIMONIALS.slice(4, 8)} direction='right' />
-                    <BrandsShowCase items={TESTIMONIALS.slice(8)} />
-                </div>
-
-            </div>
-            <ServicesSection />
-            <Inovation />
-
-            <CountryWork />
-        </>
-
-    )
-}
-
-export default HeroSection
+export default HeroSection;
